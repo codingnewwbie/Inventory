5346e5d55f37c5dfce1a55ff3086d57233303618
codingnewwbie <abyss6715@gmail.com>
2025-06-09 21:01:27 +0900

    feat: Player 정보 메서드

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,5 +8,71 @@ public class Character : MonoBehaviour
     private int _level;
     private int _exp;
     private int _gold;
+    private int _attack;
+    private int _defense;
+    private int _hp;
+    private int _critical;
+    
+    public string Name { get; private set; }
+    public int Level { get; private set; }
+    public int Exp { get; private set; }
+    public int Gold { get; private set; }
+    public int Attack { get; private set; }
+    public int Defense { get; private set; }
+    public int Hp { get; private set; }
+    public int Critical { get; private set; }
+    
+    public void IncreaseExp(int exp)
+    {
+        Exp += exp;
+    }
+    
+    public void IncreaseGold(int gold)
+    {
+        Gold += gold;
+    }
+
+    public void IncreaseAttack(int attack)
+    {
+        Attack += attack;
+    }
+    
+    public void IncreaseDefense(int defense)
+    {
+        Defense += defense;
+    }
+    
+    public void IncreaseHp(int hp)
+    {
+        Hp += hp;
+    }
+    public void IncreaseCritical(int critical)
+    {
+        Critical += critical;
+    }
+    public void DecreaseHp(int hp)
+    {
+        Hp -= hp;
+    }
+    public void DecreaseCritical(int critical)
+    {
+        Critical -= critical;
+    }
+
+    public void DecreaseAttack(int attack)
+    {
+        Attack -= attack;
+    }
+    
+    public void DecreaseDefense(int defense)
+    {
+        Defense -= defense;
+    }
+
+    public void DecreaseGold(int gold)
+    {
+        Gold -= gold;
+    }
+    
     
 }
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
+    public Character player;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        
+        player = FindObjectOfType<Character>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
